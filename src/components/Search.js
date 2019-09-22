d6b3b253e7f01dfd051feda08b320f9c0e8f80ef
rawan-youssif <55670561+rawan-youssif@users.noreply.github.com>
2019-09-23 00:10:27 +0200

    Add files via upload

---
+++ src/components/Search.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const Search = ({ search }) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchInputChanges = e => {
+    setSearchValue(e.target.value);
+  };
+
+  const resetInputField = () => {
+    setSearchValue("");
+  };
+
+  const callSearchFunction = e => {
+    e.preventDefault();
+    search(searchValue);
+    resetInputField();
+  };
+
+  return (
+    <form className="search">
+      <input
+        value={searchValue}
+        onChange={handleSearchInputChanges}
+        type="text"
+        placeholder='search for a movie'
+      />
+      <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+    </form>
+    
+  );
+};
+
+export default Search;
